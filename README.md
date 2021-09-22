b23774c856ff62ebed6a01b5299b340d21606106
JOHNSON UMAN <johnuman@ie.ibm.com>
2021-09-22 12:52:29 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Configuration Management Tool
+
+A rudimentary configuration management tool and use it to configure two servers for production service of a simple PHP web application to to Debian Linux server. CMtool is implemented similarly to Puppet, Chef, Fabric and Ansible. This tool was developed using bash / shell scripting language.
+
+## Abstractions:
+
+1.  Add bootstrap.sh program to resolve dependencies
+2.  Allows specifying a file's content and metadata (owner, group, mode)
+3.  Allows installing and removing Debian packages
+4.  Provide some mechanism for restarting a service when relevant files or packages are updated
+5.  Re-useable => Apply your configuration over and over again
+
+## How to Configure:
+
+- To install a package, add the package name to install.lst in the packages directory. Each package name should not contain own line without any trailing whitespace. See details below.
+
+`
+apache2
+php7.4-mysql
+php7.4
+php7.4-cli
+php7.4-common
+php7.4-json
+php7.4-opcache
+php7.4-zip
+php7.4-fpm
+php7.4-mbstring
+
+`
+
+- To uninstall a package involves doing the same thing as install package but this time one should add package names to the uninstall.lst file in the packages directory.
+
+- To allow specifying file content and metadata, add key value pairs to config.txt file. Each entry must be separated by "=" and have it's own line. Please make sure to remove trailing whitespace.
+
+## How to use (Tested locally on Debian Linux server 11)
+
+Before running any of the following command check whether the user is ROOT or NON ROOT user. If the user is root, then run command
+without putting sudo in front and if the user is non root then run command putting sudo in front.
+
+- Move files to remote servers via scp or ftp:
+
+  ```bash
+  scp -r CMTool.tar.gz username@IP_ADDRESS/
+  ```
+
+- Change directory to where CMTool tar file is located and untar file CMTool.tar.gz :
+
+  ```bash
+  cd /
+  tar -xvzf CMTool.tar.gz
+
+  ```
+
+- Give permissions to all files in the folder directory:
+
+  ```bash
+  chmod -R 755 CMTool
+  ```
+
+- Run CLI CMTool
+  ```bash
+  cd CMTool
+  ./main.sh
+  ```
---
+++ main.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+
+#*********************************************************************************************
+# A configuration management tool for deploying simple php application to Debian Linux server
+#*********************************************************************************************
+
+logFile="${PWD}/system.log"
+uninstallFile="${PWD}/packages/uninstall.lst"
+installFile="${PWD}/packages/install.lst"
+configFile="${PWD}/config.txt"
+appLocation="/var/www/html"
+appFile="/var/www/html/index.html"
+
+
+envOS=$(cat /etc/*-release | grep -w NAME | cut -d= -f2 | tr -d '"')
+echo $envOS
+echo "Runing Configuration Management tool"
+echo "LogFile located at: - $logFile"
+echo "Install File located at: - $installFile"
+echo "Uninstall File located at: - $uninstallFile"
+echo "Config File located at: - $configFile"
+
+# Functions:
+initialize() {
+    echo "$(date): Starting Configuration Management Tool ********************************"
+    sudo touch "${logFile}" >&/dev/null
+}
+
+bootstrapServer(){
+  echo "$(date): Bootstraping server ********************************"
+  /${PWD}/bootstrap.sh
+}
+
+
+removePackages(){
+    if [ -s $uninstallFile ];
+
+      then
+          declare -a removal_list
+          echo "$(date): Reading $uninstallFile"
+          while IFS='\n' read -r value; do
+              removal_list+=( "${value}" )
+          done < "$uninstallFile"
+
+          echo "$(date): Uninstalling packages ********************************"
+          for pkg in "${removal_list[@]}"
+          do
+
+            if dpkg -l | grep -i "${pkg}"; then
+              sudo apt-get -y remove "${pkg}"
+              sudo apt-get -y autoremove
+              sudo apt-get purge -y $(dpkg --list |grep '^rc' |awk '{print $2}')
+              sudo apt-get -y clean
+            fi
+          done
+
+
+      else
+          echo "$(date): Uninstall package list is missing entry ********************************";
+
+    fi
+
+}
+
+installPackages(){
+
+  if [ -s $installFile ];
+
+    then
+        declare -a install_list
+        echo "$(date): Reading $installFile ********************************"
+        while IFS='\n' read -r value; do
+            install_list+=( "${value}" )
+        done < "$installFile"
+
+
+        echo "$(date): Adding SURY PHP PPA repository ********************************"
+        sudo apt-get -y install lsb-release apt-transport-https ca-certificates wget
+        sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+        echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+
+        echo "$(date): Installing packages ********************************"
+        for pkg in "${install_list[@]}"
+        do
+
+          if ! dpkg -l | grep "${pkg}"; then
+            sudo apt-get install -y "${pkg}"
+          fi
+        done
+
+    else
+        echo "$(date): Install package list is missing entry ********************************";
+
+  fi
+
+
+}
+
+
+deployApp(){
+  if [ -s $appFile ]; then
+    sudo rm -rf $appFile
+  fi
+  echo "$(date): Deploying index.php to $appLocation ********************************";
+  cp ${PWD}/index.php $appLocation
+
+}
+
+
+configureApp(){
+  if [ -s $configFile ]; then
+    declare -A content
+    echo "$(date): Reading $configFile ********************************"
+    while IFS== read -r key value; do
+      content[$key]=$value
+    done < "$configFile"
+    echo "$(date): Configuring application folder ********************************"
+    sudo chmod "${content[permissions]}" "${content[file]}"
+    sudo chown "${content[owner]}" "${content[file]}"
+    sudo chgrp "${content[group]}" "${content[file]}"
+
+  fi
+
+}
+
+startApp(){
+  echo "$(date): Starting web application ********************************"
+  sudo service apache2 restart
+}
+
+
+sanityTest(){
+  echo "$(date): Running curl command  ********************************"
+  host=`hostname`
+  curlCommand= `curl -sv http://$host`;
+	echo "$curlCommand";
+}
+
+
+
+# Main
+initialize >> "${logFile}" 2>&1
+bootstrapServer >> "${logFile}" 2>&1
+removePackages >> "${logFile}" 2>&1
+installPackages >> "${logFile}" 2>&1
+deployApp >> "${logFile}" 2>&1
+configureApp >> "${logFile}" 2>&1
+startApp >> "${logFile}" 2>&1
+sanityTest >> "${logFile}" 2>&1
